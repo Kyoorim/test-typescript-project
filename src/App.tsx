2c6219d2f759af4a4504a3586a344c17244fe353
Kyoorim <kyoorim88@gmail.com>
2022-12-23 17:19:06 +0900

    ready-to-use-compon ents testing

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+// import {SimpleGrid} from "ready-to-use-components");
+
+const App = () => {
+  return (
+    <div>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      {/* <SimpleGrid column={2}>1</SimpleGrid> */}
+    </div>
+  );
+};
+
+export default App;
